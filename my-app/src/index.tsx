83e66f45887b68b21f13c713c5f4362c6c7b12da
moepwint91 <mmpwint7@gmail.com>
2023-01-22 00:20:18 +0900

    prj-setup

---
+++ my-app/src/index.tsx
@@ -0,0 +1,11 @@
+// https://reactjs.org/docs/react-dom-client.html
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import React from "react";
+
+const rootElement = document.getElementById("root");
+// https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
